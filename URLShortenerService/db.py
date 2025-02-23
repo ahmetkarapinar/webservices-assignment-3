224d0791a9e675ad4290d06ee48cd51ae6b5d7d9
ahmetkarapinar <ahmetkarapinarr00@gmail.com>
2025-02-23 19:15:25 +0100

    primary key change

--- URLShortenerService/db.py
+++ URLShortenerService/db.py
@@ -4,11 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class URLMapping(db.Model):
-    """Database model for storing URL mappings per user."""
-    id = db.Column(db.Integer, primary_key=True)
-    short_id = db.Column(db.String(2083), unique=True, nullable=False)
+    """Database model for storing URL mappings per user with a composite primary key."""
+    
+    short_id = db.Column(db.String(2083), nullable=False)
     full_url = db.Column(db.String(2083), nullable=False)
     username = db.Column(db.String(150), nullable=False)  # Store the username
+    
+    # Define composite primary key
+    __table_args__ = (
+        db.PrimaryKeyConstraint('short_id', 'username'),  # Composite primary key
+    )
 
     def __init__(self, short_id, full_url, username):
         self.short_id = short_id
